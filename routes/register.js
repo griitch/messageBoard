292f53831fcd6353c2f0f5d784a38333f160c3e6
griitch <omargrinat9@gmail.com>
2021-10-07 21:35:35 +0100

    implemented register logic and created its view

---
+++ routes/register.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const Router = express.Router();
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+
+Router.get("/", (req, res, next) => {
+  if (req.user) res.redirect("/");
+  else {
+    res.render("register", { title: "Register", error: null });
+  }
+});
+
+Router.post("/", async (req, res) => {
+  try {
+    const isAlreadyExisting = await User.findOne({
+      username: req.body.username,
+    });
+    if (isAlreadyExisting) {
+      res.render("register", {
+        title: "Register",
+        error: "This username already exists",
+      });
+    } else {
+      const hash = await bcrypt.hash(req.body.password, 5);
+      const newuser = new User({
+        username: req.body.username,
+        passwordHash: hash,
+      });
+      await newuser.save();
+      res.redirect("/");
+      // change this later to res.redirect(login)
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+module.exports = Router;
